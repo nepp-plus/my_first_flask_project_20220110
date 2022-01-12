217dff1fee3da6b0dfe74b694ee6bb53780251ef
nepp-plus <nepp.plus@gmail.com>
2022-01-12 10:14:05 +0900

    받아낸 파라미터로 연락처 추가 쿼리 작성

--- app/api/contact/contact.py
+++ app/api/contact/contact.py
@@ -1,4 +1,27 @@
+from pymysql import connect
+from pymysql.cursors import DictCursor
+
 # 연락처에 관련된 로직을 담당하는 파일.
+# db 연결 / cursor 변수
+db = connect(
+    host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com',
+    port=3306,
+    user='admin',
+    passwd='Vmfhwprxm!123',  # 프로젝트!123  첫글자만 대문자로.
+    db='test_phone_book',
+    charset='utf8',
+    cursorclass=DictCursor
+)
+
+cursor = db.cursor()
+
 
 def add_contact_to_db(params):
-    pass
+    # 연락처 추가 등록 쿼리
+    sql = f"INSERT INTO contacts (user_id, name, phone_num, memo) VALUES ({params['user_id']}, '{params['name']}',  '{params['phone']}' '{params['memo']}')"
+    print('SQL : ', sql)
+    
+    return {
+        'code': 200,
+        'message': '임시 성공 응답',
+    }
